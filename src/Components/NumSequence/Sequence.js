84c39649835e0e19b89e8b52706332028d43fff4
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-06-24 15:30:41 -0500

    adding changes: Fibonacci

---
+++ src/Components/NumSequence/Sequence.js
@@ -0,0 +1,17 @@
+import React, {Component, Fragment} from 'react';
+import './Sequence.css';
+
+
+class Sequence extends Component {
+
+      render() {
+          return(
+            <Fragment>
+            <div className="listo"></div>
+              <h1>Hola</h1>
+            </Fragment>
+          )
+      }
+  }
+  
+  export default Sequence;
