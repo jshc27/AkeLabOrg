3d5daee480bcff906c9cc5c81976ff325a8928fc
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-06-25 00:27:25 -0500

    adding functionatily to two components

--- src/Components/Fibonacci/Fibonacci.js
+++ src/Components/Fibonacci/Fibonacci.js
@@ -1,47 +1,102 @@
-import React, { Component, Fragment, useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import './Fibonacci.css';
 
-class Fibonacci extends Component {
+const Fibonacci = () => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      previous1:1,
-      previous2: 0,
-      value: ''
-    };
+  const [ arrayNumero, setarrayNumero ] = useState([])
+
+  const [ numero, setNumero] = useState(1)
+
+  const [ inputValue, setinputValue ] = useState("")
+
+  const handleChange = (event) => {
+      setinputValue(event.target.value)
   }
 
-  clickResult = (event) => {
-    console.log(event.target.type);
-    let a = this.state.previous1 +this.state.previous2;
-    this.setState({
-      previous1:a,
-      previous2:this.state.previous1
-    })
-  }
+  const handleMostrarNumeros = () => {
 
-    render() {
-        return(
-          <Fragment>
+    const secuencia = []
+
+    for(let i=1; i<= Number(inputValue); i=i+1) {
+        console.log(arrayNumero)
+        let newNumber = 1;
+        newNumber = secuencia[i] + secuencia[i + 1];
+        secuencia.push(newNumber);
+    }
+
+    setarrayNumero(secuencia);
+  }
+  return (
+      <Fragment>
           <div className="container">
               <h1>FIBONACCI</h1>
               <div className="description">
-                <p>Es una sucesión de números que inician por la unidad y cada uno
+              <p>Es una sucesión de números que inician por la unidad y cada uno
                 de sus términos es la suma de los dos anteriores (1, 1, 2, 3, 5, 8, 13, 21, ...).</p>
               </div>
               <div className="main">
-                <input  
-                  type="number" 
-                  placeholder="Ingresar numero">
-                </input>
-                <button type="submit" onClick={this.clickResult} >Mostrar</button>
-                <div className="result">{this.state.previous1}</div>
+                  <input
+                  type="text" 
+                  placeholder="Ingresar numero"
+                  onChange={handleChange}
+                  value={inputValue}
+                  /> 
+                  <button type="submit" onClick={ handleMostrarNumeros} >Mostrar</button>
+                  <div className="result">
+                      {
+                          arrayNumero.join(",")
+                      }
+                  </div>
               </div>
           </div>
-          </Fragment>
-        )
-    }
+      </Fragment>
+  );
 }
 
 export default Fibonacci;
+
+
+// class Fibonacci extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       previous1:1,
+//       previous2: 0,
+//       value: ''
+//     };
+//   }
+
+//   clickResult = (event) => {
+//     console.log(event.target.type);
+//     let a = this.state.previous1 +this.state.previous2;
+//     this.setState({
+//       previous1:a,
+//       previous2:this.state.previous1
+//     })
+//   }
+
+//     render() {
+//         return(
+//           <Fragment>
+//           <div className="container">
+//               <h1>FIBONACCI</h1>
+//               <div className="description">
+//                 <p>Es una sucesión de números que inician por la unidad y cada uno
+//                 de sus términos es la suma de los dos anteriores (1, 1, 2, 3, 5, 8, 13, 21, ...).</p>
+//               </div>
+//               <div className="main">
+//                 <input  
+//                   type="number" 
+//                   placeholder="Ingresar numero">
+//                 </input>
+//                 <button type="submit" onClick={this.clickResult} >Mostrar</button>
+//                 <div className="result">{this.state.previous1}</div>
+//               </div>
+//           </div>
+//           </Fragment>
+//         )
+//     }
+// }
+
+// export default Fibonacci;
--- src/Components/NumSequence/Sequence.js
+++ src/Components/NumSequence/Sequence.js
@@ -10,68 +10,53 @@ const Sequence = () => {
     const [ inputValue, setinputValue ] = useState("")
 
     const handleChange = (event) => {
-        // console.log(event.target.value)
         setinputValue(event.target.value)
     }
 
     const handleMostrarNumeros = () => {
 
-        // setNumero(numero + 1)
-        // // console.log('clic')
-        // setarrayNumero([
-        //     ...arrayNumero, numero
-        
-        // ])
-
         const newArray = []
 
-        for(let i=1; i<= Number(inputValue); i=i+1) {
+        for(let i=1; i <= Number(inputValue); i=i+1) {
             console.log(arrayNumero)
 
             if(i % 3 ===  0 && i % 5 === 0) {
-                newArray.push("ACDEFG")
+                newArray.push("AKELAB")
             }else if(i % 3 === 0) {
-                newArray.push("ACD")
+                newArray.push("AKE")
             } else if(i % 5 === 0) {
-                newArray.push("EFG")
+                newArray.push("LAB")
             }else {
                 newArray.push(i)
             }
         }
         
-        setarrayNumero( newArray)
+        setarrayNumero( newArray);
 
     }
 
     return (
         <Fragment>
             <div className="container">
-            <h1>Secuencia de numeros</h1>
-            <div className="description">
-                <p>Numero</p>
-            </div>
-            <div className="main">
-                <input
-                type="text" 
-                placeholder="Ingresar numero"
-                onChange={handleChange}
-                value={inputValue}
-                /> 
-                <button type="submit" onClick={ handleMostrarNumeros} >Mostrar</button>
-                <div className="result">
-
-                    {/* {
-                        arrayNumero.map((item) =>
-                            <p>{item},</p>
-                        )
-                    } */}
-                    {
-                        arrayNumero.join(",")
-                    }
-
+                <h1>Secuencia de numeros</h1>
+                <div className="description">
+                    <p>Numero</p>
+                </div>
+                <div className="main">
+                    <input
+                    type="text" 
+                    placeholder="Ingresar numero"
+                    onChange={handleChange}
+                    value={inputValue}
+                    /> 
+                    <button type="submit" onClick={ handleMostrarNumeros} >Mostrar</button>
+                    <div className="result">
+                        {
+                            arrayNumero.join(",")
+                        }
+                    </div>
                 </div>
             </div>
-          </div>
         </Fragment>
     );
 }
